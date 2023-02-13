99736f53ea1d08707dfa7e9e1f0910a1a2bf514c
taixinF <yclzbb134958861@163.com>
2023-02-13 17:25:26 +0800

    init

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,41 +1,9 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  HttpCode,
-  HttpStatus,
-  Param,
-  Patch,
-  Post,
-  Res,
-} from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
   @Get('index')
-  findAll(@Res() response) {
-    response.status(200).send('This action returns all coffees');
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return `This action returns #${id} coffee`;
-  }
-
-  @Post()
-  @HttpCode(HttpStatus.GONE)
-  create(@Body() body) {
-    return body;
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() body) {
-    return `This action updates #${id} coffee` + body;
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return `This action removes #${id} coffee`;
+  findAll() {
+    return 'this action returns all coffees';
   }
 }
