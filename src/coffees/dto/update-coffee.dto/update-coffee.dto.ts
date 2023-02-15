06594917f075aefaf2f49a73785783a3b2269d15
taixinF <yclzbb1349458861@163.com>
2023-02-15 20:44:21 +0800

    fix error

--- src/coffees/dto/update-coffee.dto/update-coffee.dto.ts
+++ src/coffees/dto/update-coffee.dto/update-coffee.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from '@nestjs/mapped-types';
 import { CreateCoffeeDto } from '../create-coffee.dto/create-coffee.dto';
+import { PartialType } from '@nestjs/mapped-types';
 
 export class UpdateCoffeeDto extends PartialType(CreateCoffeeDto) {}
