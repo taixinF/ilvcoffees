9c865bc6045b0a00d71a72bab5040e0084fd9aca
taixinF <yclzbb134958861@163.com>
2023-02-16 17:34:43 +0800

    waiting docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 //decorators（n:装饰器)
 //nest makes (extensive use of)(广泛使用) decorators
 @Module({
-  imports: [CoffeesModule],
+  imports: [
+    CoffeesModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'pass123',
+      database: 'postgres',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController], //controller : controls the invocation of the service
   providers: [AppService], //create more services to facilitate isolation
 })
--- src/coffees/dto/create-coffee.dto/create-coffee.dto.ts
+++ src/coffees/dto/create-coffee.dto/create-coffee.dto.ts
@@ -1,6 +1,6 @@
 import { IsString } from 'class-validator';
 
-export class ·CreateCoffeeDto {
+export class CreateCoffeeDto {
   @IsString()
   readonly name: string;
 
