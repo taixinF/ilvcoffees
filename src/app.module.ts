bd2d0f904fd7e4be4d39b96c8f4a12bcb12e583a
taixinF <yclzbb1349458861@163.com>
2023-02-24 07:48:32 +0800

    add

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,24 +5,25 @@ import { CoffeesModule } from './coffees/coffees.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
 import { ConfigModule } from '@nestjs/config';
-import * as Joi from '@hapi/joi';
 import appConfig from './config/app.config';
 
 @Module({
   imports: [
-    // 自定义配置文件数组
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres',
+        host: process.env.DATABASE_HOST,
+        port: +process.env.DATABASE_PORT,
+        username: process.env.DATABASE_USER,
+        // password: process.env.DATABASE_PASSWORD,
+        password: process.env.DATABASE_PASSWORD.replace(/g/g, 'G'),
+        database: process.env.DATABASE_NAME,
+        autoLoadEntities: true,
+        synchronize: true,
+      }),
+    }),
     ConfigModule.forRoot({ load: [appConfig] }),
     CoffeesModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DATABASE_HOST,
-      port: +process.env.DATABASE_PORT,
-      username: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD.replace(/g/g, 'G'),
-      database: process.env.DATABASE_NAME,
-      autoLoadEntities: true,
-      synchronize: true,
-    }),
     CoffeeRatingModule,
   ],
   controllers: [AppController],
