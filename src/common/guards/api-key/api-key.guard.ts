098194369e9b4f4dd5b30a0d330520c6bf5e6234
taixinF <yclzbb1349458861@163.com>
2023-02-28 07:18:59 +0800

    add 54

--- src/common/guards/api-key/api-key.guard.ts
+++ src/common/guards/api-key/api-key.guard.ts
@@ -1,11 +1,14 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Observable } from 'rxjs';
+import { Request } from 'express';
 
 @Injectable()
 export class ApiKeyGuard implements CanActivate {
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
-    return true;
+    const request = context.switchToHttp().getRequest<Request>();
+    const authHeader = request.header('Authorization');
+    return authHeader === process.env.API_KEY;
   }
 }
