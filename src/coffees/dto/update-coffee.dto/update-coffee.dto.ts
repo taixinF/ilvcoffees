20ddeb7c3d9501c97cc29c29a392184b806174f4
taixinF <yclzbb134958861@163.com>
2023-03-07 17:21:16 +0800

    delete

--- src/coffees/dto/update-coffee.dto/update-coffee.dto.ts
+++
@@ -1,4 +0,0 @@
-import { CreateCoffeeDto } from '../create-coffee.dto/create-coffee.dto';
-import { PartialType } from '@nestjs/mapped-types';
-
-export class UpdateCoffeeDto extends PartialType(CreateCoffeeDto) {}
--- src/coffees/repositorys/coffees.repository.ts
+++
@@ -1,5 +0,0 @@
-import { Repository, EntityRepository } from 'typeorm';
-import { Coffee } from '../entities/coffee.entity';
-
-@EntityRepository(Coffee)
-export class CoffeesRepository extends Repository<Coffee> {}
